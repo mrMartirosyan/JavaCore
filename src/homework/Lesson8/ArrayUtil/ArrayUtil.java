7b69a2bf2215db0650a7d8ba112da13c7ee7b4ec
Khachik Martirosyan <91khachikmh@gmail.com>
2022-05-15 20:38:06 +0400

    L8 - ArrayUtil

--- src/homework/Lesson8/ArrayUtil/ArrayUtil.java
+++ src/homework/Lesson8/ArrayUtil/ArrayUtil.java
@@ -2,26 +2,26 @@ package homework.Lesson8.ArrayUtil;
 
 public class ArrayUtil {
     public static void main(String[] args) {
-
-        // 2. Տպեք մասիվի ամենամեծ թիվը
-
         int[] array = {5, 15, 7, 44, 2, 16, 37, 24, 63, 11};
-        for (int i = 1; i < 10; i++) {
-            if (array[0] > array[i]) {
-                for (int j = i; j < 10; j++) {
-                    if (array[i] > array[j]) {
-                        System.out.println(array[i]);
-                    }
 
-                }
+    }
 
-                // System.out.println("Ամենամեծ թիվն է " +array[0]);
-            }
+}
 
-        }
+// 2. Տպեք մասիվի ամենամեծ թիվը - ՉՍՏԱՑՎԱԾ 1 փորձ
+
+//       int[] array = {5, 15, 7, 44, 2, 16, 37, 24, 63, 11};
+//       for (int i = 1; i < 10; i++) {
+//           if (array[0] > array[i]) {
+//              for (int j = i; j < 10; j++) {
+//                if (array[i] > array[j]) {
+//                   System.out.println(array[i]);
+//                    }
+//
+//                }
 
 
-        // 1. Տպեք մասիվի բոլոր էլեմենտները
+// 1. Տպեք մասիվի բոլոր էլեմենտները
 
 //        int[] array = {5, 15, 7, 44, 2, 16, 37, 24, 63, 11};
 //        for (int i = 0; i < 10; i++) {
@@ -30,4 +30,4 @@ public class ArrayUtil {
     }
 
 
-}
+            }
