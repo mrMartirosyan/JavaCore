da8abc6e18a743e54df4034f4a83b5c77ac30141
Khachik Martirosyan <91khachikmh@gmail.com>
2022-05-15 21:00:09 +0400

    L8 - ArrayUtil

--- src/homework/Lesson8/ArrayUtil/ArrayUtil.java
+++ src/homework/Lesson8/ArrayUtil/ArrayUtil.java
@@ -3,10 +3,21 @@ package homework.Lesson8.ArrayUtil;
 public class ArrayUtil {
     public static void main(String[] args) {
         int[] array = {5, 15, 7, 44, 2, 16, 37, 24, 63, 11};
-
+        for (int i = 0; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                if (array[i] > array[j]) {
+                    for (int k = j + 1; k < 10; k++) {
+                        if (array[j] > array[k]) {
+                            System.out.println(array[j]);
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
+
 
 // 2. Տպեք մասիվի ամենամեծ թիվը - ՉՍՏԱՑՎԱԾ 1 փորձ
 
