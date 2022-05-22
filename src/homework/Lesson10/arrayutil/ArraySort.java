2ab1179b03464b80ba9a87a7f32ef50416da8fb8
Khachik Martirosyan <91khachikmh@gmail.com>
2022-05-22 14:20:33 +0400

    L8 - ArrayUtil

---
+++ src/homework/Lesson10/arrayutil/ArraySort.java
@@ -0,0 +1,30 @@
+package homework.Lesson10.arrayutil;
+
+public class ArraySort {
+    public static void main(String[] args) {
+
+        int[] numbers = {43, 55, 5, -9, 0, 12, 5, 65};
+        int duty;
+
+        for (int x = 0; x < numbers.length * numbers.length; x++) {
+
+            for (int i = 0; i < numbers.length - 1; i++) {
+                for (int j = i + 1; j < i + 2 || j < numbers.length; j += i) {
+
+                    if (numbers[i] < numbers[j]) {
+                        break;
+                    }
+                    if (numbers[i] > numbers[j]) {
+                        duty = numbers[j];
+                        numbers[j] = numbers[i];
+                        numbers[i] = duty;
+                    }
+                }
+            }
+        }
+        for (int z = 0; z < numbers.length; z++) {
+            System.out.print(numbers[z] + " ");
+        }
+
+    }
+}
